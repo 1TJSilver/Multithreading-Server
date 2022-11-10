ef2ef5d5ffe2b28db9a38178f862d67a22466307
John Terve <89539449797@mail.ru>
2022-11-10 18:35:51 +0300

    Add x-www-form-urlencoded

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
@@ -13,25 +14,43 @@ public class Request {
     private String method;
     private String path;
     private String body;
-    public Request(String method, String path, Map<String, ConcurrentHashMap<String, Handler>> handlers){
+
+    public Request(String method, String path, Map<String, ConcurrentHashMap<String, Handler>> handlers) {
         this.path = path;
         this.method = method;
         nameValueParams = getQueryParams();
     }
+
     public Request(String method, String path, String body,
-                   Map<String, ConcurrentHashMap<String, Handler>> handlers){
+                   Map<String, ConcurrentHashMap<String, Handler>> handlers) {
         this.path = path;
         this.method = method;
         this.body = body;
         nameValueParams = getQueryParams();
     }
-    public List<NameValuePair> getQueryParams(){
-        return URLEncodedUtils.parse(path, StandardCharsets.UTF_8);
+
+    public List<NameValuePair> getPostParams() {
+        return URLEncodedUtils.parse(body, StandardCharsets.UTF_8);
     }
-    public List<String> getQueryParam(String name){
+
+    public List<String> getPostParam(String name) {
         List<String> result = new ArrayList<>();
         for (NameValuePair pair : nameValueParams) {
-            if (pair.getName().equals(name)){
+            if (pair.getName().equals(name)) {
+                result.add(pair.getValue());
+            }
+        }
+        return result;
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return URLEncodedUtils.parse(path, StandardCharsets.UTF_8);
+    }
+
+    public List<String> getQueryParam(String name) {
+        List<String> result = new ArrayList<>();
+        for (NameValuePair pair : nameValueParams) {
+            if (pair.getName().equals(name)) {
                 result.add(pair.getValue());
             }
         }
